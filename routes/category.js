771456ecd96f031eea33ea676b32b51e11bc24c6
Paul Kitonyi <paulmucimah@gmail.com>
2019-10-24 11:51:08 +0300

    Add get categoryById route

--- routes/category.js
+++ routes/category.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/category.js");
+const { create, categoryById, read } = require("../controllers/category.js");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
 router.post("/category/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.get("/category/:categoryId", read);
 
-
+router.param("categoryId", categoryById);
 router.param("userId", userById);
 
 module.exports = router;
