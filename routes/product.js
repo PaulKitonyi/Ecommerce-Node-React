78fe740556620d0b05cce72e59f90c8690693dd0
Paul Kitonyi <paulmucimah@gmail.com>
2019-10-25 17:12:42 +0300

    Add relatedproducts route

--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, productById, read, remove, update} = require("../controllers/product");
+const { create, productById, read, remove, update, list, listRelated} = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
 router.get("/product/:productId", read);
+router.get("/products", list);
+router.get("/products/related/:productId", listRelated);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
 router.delete("/product/:productId/:userId", requireSignin, isAuth, isAdmin, remove);
 router.put("/product/:productId/:userId", requireSignin, isAuth, isAdmin, update);
